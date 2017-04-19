58daf0d2a4aef5ba5ccdbdf9c4db37e15f69c2ad
Julien Bonnardel <julien.bonnardel94@gmail.com>
2017-04-19 19:17:44 +0200

    Avancement

--- src/EasygestionBundle/Controller/AccueilController.php
+++ src/EasygestionBundle/Controller/AccueilController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AccueilController extends Controller
 {
+    public function loginAction(){
+        die('ici');
+    }
+    
     public function accueilAction()
     {
         return $this->render('EasygestionBundle:Default:accueil.html.twig');
---
+++ src/EasygestionBundle/Form/LoginType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace EasygestionBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginType extends AbstractType
+{
+    public fonction buildForm(FormBuilderInterface $builder, array $options)
+    {
+        
+    }
+    
+}
