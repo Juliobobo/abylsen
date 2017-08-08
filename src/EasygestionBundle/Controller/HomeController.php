391b11f2f6a590817d403022d23a8cb1ff8130a1
Julien Bonnardel <julien.bonnardel94@gmail.com>
2017-08-08 12:09:43 +0200

    homepage done

--- src/EasygestionBundle/Controller/HomeController.php
+++ src/EasygestionBundle/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace EasygestionBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Class HomeController
@@ -21,5 +22,10 @@ class HomeController extends Controller
      public function indexAction()
     {
         return $this->render('EasygestionBundle:Home:home.html.twig');
+    }
+    
+    public function getParent()
+    {
+        return 'FOSUserBundle';
     }
 }
